bcf708af4d7046b20325e6ecb9d39df407603653
Nithin <nithindevathi@gmail.com>
2023-11-01 18:45:19 +0530

    Filters Impleted

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,6 +6,7 @@ import {
   setRating,
   setSort,
   setAllClear,
+  setRange,
 } from "../Redux/FilterSlice";
 const Filters = () => {
   const dispatch = useDispatch();
@@ -15,49 +16,77 @@ const Filters = () => {
     dispatch(FetchCategory());
   }, [dispatch]);
   return (
-    <div>
+    <div className="font-nunito border-r-2 border-blue-950 h-100vh p-4 max-sm:hidden">
       <div className="flex gap-16">
-        <p>Filters</p>
-        <button onClick={() => dispatch(setAllClear())}>Clear</button>
+        <p className="text-xl">Filters</p>
+        <button
+          className="text-xl font-normal text-blue-500"
+          onClick={() => dispatch(setAllClear())}
+        >
+          Clear
+        </button>
+      </div>
+      <div>
+        <p>Price Range</p>
+        <input
+          id="large-range"
+          type="range"
+          min="0"
+          max="3800001"
+          onChange={(e) => dispatch(setRange(e.target.value))}
+          class="w-full h-1 bg-stone-500  rounded-lg appearance-none cursor-pointer range-lg "
+        />
       </div>
       <hr></hr>
-      <p>Category By</p>
-      {categoriesList?.map((cate) => (
-        <div key={cate.categoryName} className="space-x-2">
-          <label>
+      <div className="p-3">
+        <p className="text-lg">Category By</p>
+        {categoriesList?.map((cate) => (
+          <label key={cate.categoryName} className="space-x-2 flex">
             <input
               type="checkbox"
               onClick={() => dispatch(setCategory(cate.categoryName))}
               checked={filtersList.CategoryFilter.includes(cate.categoryName)}
             />
-            {cate.categoryName}
+            <span>{cate.categoryName}</span>
           </label>
-        </div>
-      ))}
-      <p>Rating </p>
-      {Array.from({ length: 5 }, (_, i) => (
-        <div key={i} className="space-x-2 flex items-center">
+        ))}
+      </div>
+      <hr></hr>
+      <ul className="p-3 w-max">
+        <p className="text-lg">Rating </p>
+        {Array.from({ length: 5 }, (_, i) => (
+          <li key={i} className="space-x-2 flex items-center">
+            <input
+              id="default-radio-1"
+              type="radio"
+              value={i + 1}
+              className="w-4 h-4"
+              onClick={() => dispatch(setRating(i + 1))}
+              checked={i + 1 === filtersList.Rating}
+            />
+            <label>{i + 1} Stars and Above</label>
+          </li>
+        ))}
+      </ul>
+      <hr></hr>
+      <div className="flex flex-col p-3">
+        <p className="text-lg">Sorty By</p>
+        <label className="space-x-2">
           <input
-            id="default-radio-1"
             type="radio"
-            value={i + 1}
-            name="default-radio"
-            className="w-4 h-4"
-            onClick={() => dispatch(setRating(i + 1))}
-          />
-          <label>{i + 1} Stars and Above</label>
-        </div>
-      ))}
-      <p>Sorty By</p>
-      <div className="flex flex-col">
-        <label>
-          <input type="radio" onClick={() => dispatch(setSort("l2h"))}></input>
-          Low to High
+            checked={filtersList.SortBy == "l2h"}
+            onClick={() => dispatch(setSort("l2h"))}
+          ></input>
+          <span>Low to High</span>
         </label>
 
-        <label>
-          <input type="radio" onClick={() => dispatch(setSort("h2l"))}></input>
-          High to Low
+        <label className="space-x-2">
+          <input
+            type="radio"
+            checked={filtersList.SortBy == "h2l"}
+            onClick={() => dispatch(setSort("h2l"))}
+          ></input>
+          <span>High to Low</span>
         </label>
       </div>
     </div>
