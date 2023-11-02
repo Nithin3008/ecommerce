9590e8af674458ab7ee04f69d3ea17d73aa94bfd
Nithin <nithindevathi@gmail.com>
2023-11-02 11:17:38 +0530

    Create HelperFunctions.js

---
+++ src/services/HelperFunctions.js
@@ -0,0 +1,49 @@
+import store from "../Redux/Store";
+
+export const itemInCart = (id) => {
+  const CartData = store.getState().cart.cart;
+  const product = CartData?.find((val) => (val._id === id ? val.qty : false));
+  if (product === undefined) {
+    return 0;
+  } else {
+    return id;
+  }
+};
+export const itemInWishList = (id) => {
+  const WhisListData = store.getState().wishlist.wishlist;
+  const product = WhisListData?.find((val) =>
+    val._id === id ? val.id : false
+  );
+  if (product === undefined) {
+    return 0;
+  } else {
+    return id;
+  }
+};
+export function ApplyFilters(ProdDetails) {
+  const filtersData = store.getState().filter;
+  let newData = [...ProdDetails];
+  if (filtersData.Search !== "") {
+    newData = newData.filter((val) =>
+      val.title.toLowerCase().includes(filtersData.Search)
+    );
+  }
+  if (filtersData.CategoryFilter.length > 0) {
+    console.log(filtersData.CategoryFilter);
+    newData = newData.filter((val) =>
+      filtersData.CategoryFilter.find((item) => item === val.categoryName)
+    );
+  }
+  if (filtersData.SortBy === "l2h") {
+    newData = newData.sort((a, b) => a.price - b.price);
+  }
+  if (filtersData.SortBy === "h2l") {
+    newData = newData.sort((a, b) => b.price - a.price);
+  }
+  if (filtersData.Rating > 0) {
+    newData = newData.filter((val) => val.rating >= filtersData.Rating);
+  }
+  // if (filtersData.Range < 4000000)
+  console.log(newData);
+  return newData;
+}
