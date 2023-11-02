8334688f9cbf514011344a8a11a2202952b183a7
Nithin <nithindevathi@gmail.com>
2023-11-02 11:52:09 +0530

    refactor: code formatted

--- src/pages/ProductSummary.jsx
+++ src/pages/ProductSummary.jsx
@@ -7,6 +7,7 @@ import { AddCartItem } from "../Redux/CartSlice";
 import { itemInCart, itemInWishList } from "../services/HelperFunctions";
 import { AddWishlistItem } from "../Redux/WishlistRedux";
 import Button from "../components/Button";
+import NavBar from "../components/NavBar";
 const ProductSummary = () => {
   const { prodId } = useParams();
   const dispatch = useDispatch();
@@ -16,49 +17,52 @@ const ProductSummary = () => {
   const data = productsList?.find((val) => val._id == prodId);
   console.log(data);
   return (
-    <div className="flex justify-center mt-56 gap-5 p-4 ">
-      <div>
-        <img
-          src={data.src}
-          className=" h-80 object-fit rounded"
-          alt="car"
-        ></img>
-      </div>
-      <div className="space-y-4">
-        <h1 className="text-2xl">Company: {data.manufacturer}</h1>
-        <h2 className="text-xl">Name: {data.title}</h2>
-        <p className="text-xl">
-          Rating: {data.rating}{" "}
-          <FontAwesomeIcon icon={faStar} style={{ color: "#f9f462" }} />
-        </p>
-        <hr></hr>
-        <p className="text-xl">Hp: {data.HP}</p>
-        <p className="text-xl">Torque: {data.Torque}</p>
-        <p className="text-xl">Price: {data.price}</p>
-        <div className="space-x-2">
-          <Button
-            isdisabled={itemInCart(data._id) == data._id}
-            onReact={() => dispatch(AddCartItem(data))}
-            styling={
-              "border-2 border-blue-500 p-1 rounded text-blue-500 hover:bg-blue-500 hover:text-white"
-            }
-          >
-            {itemInCart(data._id) == data._id ? "Go to Cart" : "Add to Cart"}
-          </Button>
-          <Button
-            onReact={() => dispatch(AddWishlistItem(data))}
-            isdisabled={itemInWishList(data._id) == data._id}
-            styling={
-              "border-2 border-blue-500 p-1 rounded text-blue-500 hover:bg-blue-500 hover:text-white"
-            }
-          >
-            {itemInWishList(data._id) == data._id
-              ? "Go to Wishlist"
-              : "Add to WishList"}
-          </Button>
+    <>
+      <NavBar></NavBar>
+      <div className="flex justify-center mt-44 gap-5 p-4 ">
+        <div>
+          <img
+            src={data.src}
+            className=" h-80 object-fit rounded"
+            alt="car"
+          ></img>
+        </div>
+        <div className="space-y-4">
+          <h1 className="text-2xl">Company: {data.manufacturer}</h1>
+          <h2 className="text-xl">Name: {data.title}</h2>
+          <p className="text-xl">
+            Rating: {data.rating}{" "}
+            <FontAwesomeIcon icon={faStar} style={{ color: "#f9f462" }} />
+          </p>
+          <hr></hr>
+          <p className="text-xl">Hp: {data.HP}</p>
+          <p className="text-xl">Torque: {data.Torque}</p>
+          <p className="text-xl">Price: {data.price}</p>
+          <div className="space-x-2">
+            <Button
+              isdisabled={itemInCart(data._id) == data._id}
+              onReact={() => dispatch(AddCartItem(data))}
+              styling={
+                "border-2 border-blue-500 p-1 rounded text-blue-500 hover:bg-blue-500 hover:text-white"
+              }
+            >
+              {itemInCart(data._id) == data._id ? "Go to Cart" : "Add to Cart"}
+            </Button>
+            <Button
+              onReact={() => dispatch(AddWishlistItem(data))}
+              isdisabled={itemInWishList(data._id) == data._id}
+              styling={
+                "border-2 border-blue-500 p-1 rounded text-blue-500 hover:bg-blue-500 hover:text-white"
+              }
+            >
+              {itemInWishList(data._id) == data._id
+                ? "Go to Wishlist"
+                : "Add to WishList"}
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
