85e5a1b394b1c5e79f4373f201f44402ba789cc0
Nithin <nithindevathi@gmail.com>
2023-11-02 11:17:31 +0530

    feat: Fetch and add Wishlist items using api
    Fixed the code

--- src/Redux/WishlistRedux.js
+++ src/Redux/WishlistRedux.js
@@ -1,16 +1,60 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// const initialState = { cart: [] };
-// const wishListSlice = createSlice({
-//   name: "wishlist",
-//   initialState,
-//   reducers: {
-//     fetchWishlist(state) {
-//       state.cart = action.payload;
-//     },
-//     // addNewItemToWishlist(state) {
-//     //   state.cart.push(action.payload);
-//     // },
-//   },
-// });
-// export const { fetchWishlist } = wishListSlice.actions;
-// export default wishListSlice.reducer;
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import axios from "axios";
+
+const initialState = { wishlist: [], status: "idle" };
+const wishListSlice = createSlice({
+  name: "wishlist",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(FetchWishlist.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(FetchWishlist.fulfilled, (state, action) => {
+      state.wishlist = action.payload;
+      state.status = "idle";
+    });
+
+    builder.addCase(AddWishlistItem.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(AddWishlistItem.fulfilled, (state, action) => {
+      state.wishlist = action.payload;
+      state.status = "idle";
+    });
+  },
+});
+export const FetchWishlist = createAsyncThunk(
+  "Wishlist/FetchWishlist",
+  async () => {
+    const encodedToken = localStorage.getItem("loginToken");
+
+    const response = await axios.get(`/api/user/wishlist`, {
+      headers: {
+        authorization: encodedToken,
+      },
+    });
+    return response.data.wishlist;
+  }
+);
+export const AddWishlistItem = createAsyncThunk(
+  "Wishlist/AddWishlistItem",
+  async (product, thunkAPI) => {
+    const encodedToken = localStorage.getItem("loginToken");
+    const response = await axios.post(
+      "/api/user/wishlist",
+      {
+        product,
+      },
+      {
+        headers: {
+          authorization: encodedToken,
+        },
+      }
+    );
+    return response.data.wishlist;
+  }
+);
+
+export default wishListSlice.reducer;
