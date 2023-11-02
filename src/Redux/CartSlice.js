e5a782c1d9163538e74687b08205b3bc2304e360
Nithin <nithindevathi@gmail.com>
2023-11-02 11:15:57 +0530

    feat: Fetch and Add Cart items using api

--- src/Redux/CartSlice.js
+++ src/Redux/CartSlice.js
@@ -1,16 +1,55 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// const initialState = { cart: [] };
-// const cartSlice = createSlice({
-//   name: "cart",
-//   initialState,
-//   reducers: {
-//     fetchCart(state) {
-//       state.cart = action.payload;
-//     },
-//     addNewItemToCart(state) {
-//       state.cart.push(action.payload);
-//     },
-//   },
-// });
-// export const { fetchCart, addNewItemToCart } = cartSlice.actions;
-// export default cartSlice.reducer;
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+const initialState = { cart: [], status: "idle" };
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(FetchCart.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(FetchCart.fulfilled, (state, action) => {
+      state.cart = action.payload;
+      state.status = "idle";
+    });
+
+    builder.addCase(AddCartItem.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(AddCartItem.fulfilled, (state, action) => {
+      state.cart = action.payload;
+      state.status = "idle";
+    });
+  },
+});
+export const FetchCart = createAsyncThunk("cart/FetchCart", async () => {
+  const encodedToken = localStorage.getItem("loginToken");
+
+  const response = await axios.get(`/api/user/cart`, {
+    headers: {
+      authorization: encodedToken,
+    },
+  });
+  console.log("cart loading", response.data);
+});
+export const AddCartItem = createAsyncThunk(
+  "cart/AddCartItem",
+  async (product, thunkAPI) => {
+    const encodedToken = localStorage.getItem("loginToken");
+    console.log(product);
+    const response = await axios.post(
+      "/api/user/cart",
+      {
+        product,
+      },
+      {
+        headers: {
+          authorization: encodedToken,
+        },
+      }
+    );
+    return response.data.cart;
+  }
+);
+export default cartSlice.reducer;
