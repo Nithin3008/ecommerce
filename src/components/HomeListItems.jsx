9a0e67eb0aa069f484d49ab15768dddd461a9faa
Nithin <nithindevathi@gmail.com>
2023-11-03 12:25:41 +0530

    feat: Users can interact from home

--- src/components/HomeListItems.jsx
+++ src/components/HomeListItems.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setAllClear, setCategory } from "../Redux/FilterSlice";
 const HomeListItems = ({ data }) => {
+  const dispatch = useDispatch();
+  const nav = useNavigate();
+  function selectCategory(category) {
+    dispatch(setAllClear());
+    dispatch(setCategory(category));
+
+    nav("/Products");
+  }
   return (
     <li className="flex flex-col items-center gap-2 font-nunito">
       <img
         className="h-52 rounded-lg object-cover max-sm:h-56"
         src={data.src}
         alt="car"
+        onClick={() => selectCategory(data.categoryName)}
       ></img>
       <p className="text-xl ">{data.categoryName}</p>
     </li>
