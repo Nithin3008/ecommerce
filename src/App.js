faf577dd1b43f338eea4861db5f73300965af2f9
Nithin <nithindevathi@gmail.com>
2023-11-03 12:12:31 +0530

    feat: Added new Routes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import ProductSummary from "./pages/ProductSummary";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
 function App() {
   return (
     <div className="App">
-      <Product></Product>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/Login" element={<Login></Login>}></Route>
         <Route path="/Signup" element={<Signup></Signup>}></Route>
-      </Routes> */}
+        <Route
+          path="/ProductSummary/:prodId"
+          element={<ProductSummary></ProductSummary>}
+        ></Route>
+        <Route path="/Products" element={<Product></Product>}></Route>
+        <Route path="/Cart" element={<Cart></Cart>}></Route>
+        <Route path="/Wishlist" element={<Wishlist></Wishlist>}></Route>
+      </Routes>
     </div>
   );
 }
