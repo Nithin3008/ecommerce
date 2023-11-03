7bcccc0b3e9e976c19c740bd3b56cf04e743f5ba
Nithin <nithindevathi@gmail.com>
2023-11-03 12:11:38 +0530

    feat: Added new functionality

--- src/Redux/WishlistRedux.js
+++ src/Redux/WishlistRedux.js
@@ -23,6 +23,13 @@ const wishListSlice = createSlice({
       state.wishlist = action.payload;
       state.status = "idle";
     });
+    builder.addCase(RemoveWishlistItem.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(RemoveWishlistItem.fulfilled, (state, action) => {
+      state.wishlist = action.payload;
+      state.status = "idle";
+    });
   },
 });
 export const FetchWishlist = createAsyncThunk(
@@ -53,6 +60,20 @@ export const AddWishlistItem = createAsyncThunk(
         },
       }
     );
+    return response.data.wishlist;
+  }
+);
+export const RemoveWishlistItem = createAsyncThunk(
+  "Wishlist/RemoveWishlistItem",
+  async (prodId, thunkAPI) => {
+    console.log(prodId);
+    const encodedToken = localStorage.getItem("loginToken");
+    const response = await axios.delete(`/api/user/wishlist/${prodId}`, {
+      headers: {
+        authorization: encodedToken,
+      },
+    });
+    console.log(response.data);
     return response.data.wishlist;
   }
 );
