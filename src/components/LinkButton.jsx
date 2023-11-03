093d55ee339550ce916c33aa38db860a6c920f3d
Nithin <nithindevathi@gmail.com>
2023-11-03 12:04:50 +0530

    style: Added styling

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-const LinkButton = ({ children, to, style }) => {
+const LinkButton = ({ children, to, styling }) => {
   return (
-    <NavLink to={to} className={style}>
+    <NavLink
+      style={({ isActive }) => {
+        return {
+          fontWeight: isActive ? "bold" : "",
+          color: isActive ? "#0047AB" : "black",
+          fontSize: isActive ? "1.4rem" : "",
+        };
+      }}
+      to={to}
+      className={styling}
+    >
       {children}
     </NavLink>
   );
