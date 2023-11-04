30855c8718c92ab8c3724dd1ef71a85659d5e777
Nithin <nithindevathi@gmail.com>
2023-11-04 12:49:40 +0530

    Update Store.js

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./CartSlice";
 import prodCateReducer from "./ProductCategorySlice";
-import UserReducer from "./UserRedux";
+import userReducer from "./UserSlice";
 import wishListReducer from "./WishlistRedux";
 import FiltersReducer from "./FilterSlice";
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     prodCate: prodCateReducer,
-    // User: UserReducer,
+    user: userReducer,
     wishlist: wishListReducer,
     filter: FiltersReducer,
   },
