2d829382cb726390b85f84f0d3c615baec93c0f6
Nithin <nithindevathi@gmail.com>
2023-11-04 17:41:22 +0530

    feat: Made changes to re use

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -5,33 +5,55 @@ import Modal from "./Modal";
 import EditAddress from "./EditAddress";
 import { useDispatch } from "react-redux";
 import { removeAddress } from "../Redux/UserSlice";
+import { useLocation } from "react-router-dom";
 
-const Address = ({ data }) => {
+const Address = ({ data, CheckoutAddr, addrVerify }) => {
   const [showEdit, setEdit] = useState(false);
   const dispatch = useDispatch();
+  const location = useLocation();
+
   function closeForm() {
     setEdit(false);
   }
   return (
-    <li className="border-2 border-stone-500 p-4">
-      <p className="text-xl">Street: {data.street}</p>
-      <p className="text-xl">Pincode: {data.code}</p>
-      <p className="text-xl">City: {data.city}</p>
-      <p className="text-xl">Country: {data.country}</p>
-      <div className="space-x-2 space-y-1">
-        <Button
-          onReact={() => setEdit((t) => !t)}
-          styling={"text-lg text-white rounded p-1 px-2 bg-blue-500"}
-        >
-          Edit
-        </Button>
-        <Button
-          onReact={() => dispatch(removeAddress(data.id))}
-          styling={"text-lg text-white rounded p-1 px-2 bg-red-500"}
-        >
-          Delete
-        </Button>
+    <li
+      className={`border-2 border-stone-500 p-4 w-96 ${
+        location.pathname === "/Checkout" ? "flex items-center gap-4" : ""
+      }`}
+    >
+      {location.pathname === "/Checkout" && (
+        <div>
+          <input
+            required
+            onClick={() => CheckoutAddr(data.id)}
+            type="radio"
+            checked={addrVerify === data.id}
+          ></input>
+        </div>
+      )}
+      <div>
+        <p className="text-xl">Street: {data.street}</p>
+        <p className="text-xl">Pincode: {data.code}</p>
+        <p className="text-xl">City: {data.city}</p>
+        <p className="text-xl">Country: {data.country}</p>
+        {location.pathname === "/Profile" && (
+          <div className="space-x-2 space-y-1">
+            <Button
+              onReact={() => setEdit((t) => !t)}
+              styling={"text-lg text-white rounded p-1 px-2 bg-blue-500"}
+            >
+              Edit
+            </Button>
+            <Button
+              onReact={() => dispatch(removeAddress(data.id))}
+              styling={"text-lg text-white rounded p-1 px-2 bg-red-500"}
+            >
+              Delete
+            </Button>
+          </div>
+        )}
       </div>
+
       {showEdit && (
         <Modal>
           <EditAddress data={data} closeForm={() => closeForm()}></EditAddress>
