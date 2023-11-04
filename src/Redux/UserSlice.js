2ebda46c68aa017c839f8527f06857e6baa5ff6e
Nithin <nithindevathi@gmail.com>
2023-11-04 12:49:51 +0530

    Create UserSlice.js

---
+++ src/Redux/UserSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuid } from "uuid";
+const initialState = {
+  isLogin: false,
+  user: {
+    fName: "",
+    lName: "",
+    userName: "",
+  },
+  address: [
+    {
+      id: uuid(),
+      phoneNo: 7894561230,
+      street: "Wall street lower manhattam",
+      code: 10005,
+      city: "New York",
+      country: "USA",
+    },
+  ],
+};
+const UserSlice = createSlice({
+  name: "User",
+  initialState,
+  reducers: {
+    setLogin(state, action) {
+      state.user.fName = action.payload.fName;
+      state.user.lName = action.payload.lName;
+      state.user.userName = action.payload.userName;
+      state.isLogin = true;
+    },
+    setLogout(state) {
+      state.user.fName = "";
+      state.user.lName = "";
+      state.user.userName = "";
+      state.isLogin = false;
+    },
+    addAddress(state, action) {
+      state.address = [...state.address, action.payload];
+    },
+    removeAddress(state, action) {
+      state.address = state.address.filter((val) => val.id !== action.payload);
+    },
+    editAdress(state, action) {
+      console.log(action.payload);
+      state.address = state.address.map((addr) =>
+        addr.id == action.payload.id ? { ...action.payload } : addr
+      );
+    },
+  },
+});
+export const { setLogin, addAddress, removeAddress, setLogout, editAdress } =
+  UserSlice.actions;
+export default UserSlice.reducer;
