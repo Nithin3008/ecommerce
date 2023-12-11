fb611cd2f737e093c6356d9ca9f0d8ab6b40b32c
Nithin <nithindevathi@gmail.com>
2023-12-11 19:50:38 +0530

    Added loader functionality

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,8 +4,9 @@ import Filters from "../components/Filters";
 import { useDispatch, useSelector } from "react-redux";
 import { FetchProducts } from "../Redux/ProductCategorySlice";
 import ProductsListItems from "../components/ProductsListItems";
-import { FetchCart } from "../Redux/CartSlice";
-import { FetchWishlist } from "../Redux/WishlistRedux";
+// import { FetchCart } from "../Redux/CartSlice";
+// import { FetchWishlist } from "../Redux/WishlistRedux";
+import { Loader } from "../components/Loader";
 import { ApplyFilters } from "../services/HelperFunctions";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -18,6 +19,7 @@ const Product = () => {
   const productsList = ApplyFilters(productsArr);
   const filtersPage = useSelector((state) => state.filter);
   const [showFilter, setFilter] = useState(false);
+  console.log(productsList);
   return (
     <>
       <NavBar></NavBar>
@@ -26,14 +28,18 @@ const Product = () => {
         onClick={() => setFilter((s) => !s)}
         icon={faBars}
       />
-      <section className="flex ">
-        <Filters show={showFilter}></Filters>
-        <ul className="flex flex-wrap flex-1 gap-5 items-center justify-center mt-7">
-          {productsList?.map((val) => (
-            <ProductsListItems key={val._id} data={val}></ProductsListItems>
-          ))}
-        </ul>
-      </section>
+      {productsList.length > 0 ? (
+        <section className="flex ">
+          <Filters show={showFilter}></Filters>
+          <ul className="flex flex-wrap flex-1 gap-5 items-center justify-center mt-7">
+            {productsList?.map((val) => (
+              <ProductsListItems key={val._id} data={val}></ProductsListItems>
+            ))}
+          </ul>
+        </section>
+      ) : (
+        <Loader></Loader>
+      )}
     </>
   );
 };
