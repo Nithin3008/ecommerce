d65ace6005a279b89d83a75f615fdd95d973e76d
Nithin <nithindevathi@gmail.com>
2023-12-16 18:48:08 +0530

    Revert "minor fix in filters"
    This reverts commit d1a8ab0d688bf16d9957c4dbcc5686d4db9baf5a.

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -17,7 +17,8 @@ const Filters = ({ show }) => {
   }, [dispatch]);
   return (
     <div
-      className={`font-nunito border-r-2 border-blue-950 h-100%  p-4 max-sm:hidden
+      className={`font-nunito border-r-2 border-blue-950 h-100%  p-4 max-sm:${
+        show ? "inline" : "hidden"
       } space-y-4 `}
     >
       <div className="flex gap-16">
